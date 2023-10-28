b1a6dee8237bb46eac1322914f8bed66a10a2656
PavanKumarYarramsetti <105154576+PavanKumarYarramsetti@users.noreply.github.com>
2023-10-28 16:12:29 +0530

    log lines

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from flask import Flask
+import logging
 app = Flask(__name__)
  
 @app.route('/')
 def main_route():
-    return 'Hello World!'
- 
+ logging.info("in main route")
+ return 'Hello World!'
+
+@app.route('/home')
+def second():
+ logging.info("in home route")
+ return "Bye."
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8080)
